3d2a01ca4b396146b22e9077ee77ab180149f8b7
Silvia Collicelli <collicellisilvia@gmail.com>
2025-11-09 18:34:09 +0100

    adding training and validation per patient

--- dataset2.py
+++ dataset2.py
@@ -68,10 +68,10 @@ class MyDataset(Dataset):
         self.transform = transform
         self.target_transform = target_transform
 
-        case_folders = [entry.name for entry in os.scandir(self.img_dir) if entry.is_dir()]
+        self.case_folders = [entry.name for entry in os.scandir(self.img_dir) if entry.is_dir()]
 
-        for i in range(len(case_folders)):
-            case_path = os.path.join(self.img_dir, case_folders[i])
+        for i in range(len(self.case_folders)):
+            case_path = os.path.join(self.img_dir, self.case_folders[i])
 
             for entry in os.scandir(case_path):
                 item_folder_path = os.path.join(case_path, entry.name)
@@ -79,7 +79,7 @@ class MyDataset(Dataset):
                 for item_entry in os.scandir(item_folder_path):
 
                     if item_entry.name.startswith(entry.name) and (item_entry.name.endswith(('.jpeg', '.png'))):    #if entry is an image file
-                        self.samples.append((item_entry.path, self.labels_dict[img_labels[case_folders[i]]], case_folders[i]))
+                        self.samples.append((item_entry.path, self.labels_dict[img_labels[self.case_folders[i]]], self.case_folders[i]))
 
                     #if item_entry.is_dir() and item_entry.name!='00000' and item_entry.name!='frames' and not_all_frames:
                     #    idx_des_frames.append(int(item_entry.name))
@@ -88,7 +88,7 @@ class MyDataset(Dataset):
                         #idx_des_frames=sorted(idx_des_frames)
                         frames = video_to_tensors(item_entry.path, frame_skip)
                         for frame in frames:
-                            self.samples.append((frame, self.labels_dict[img_labels[case_folders[i]]], case_folders[i]))
+                            self.samples.append((frame, self.labels_dict[img_labels[self.case_folders[i]]], self.case_folders[i]))
                     #    for s in range(idx):
                     #        self.img_paths.append(os.path.join(frames_path, entry.name, f"frame_{idx_des_frames[s]:04d}.jpeg"))
                     #        self.label_files.append(self.labels_dict[img_labels[case_folders[i]]])
@@ -108,12 +108,28 @@ class MyDataset(Dataset):
         return image, label
     
 
-clinical_path = r"raw_dataset/artifacts/clinical_case_metadata.parquet"
-folder_path = r"raw_dataset/artifacts/Dataset"
-frames_path = r"/home/silvia.collicelli/frames"
+#clinical_path = r"raw_dataset/artifacts/clinical_case_metadata.parquet"
+#folder_path = r"raw_dataset/artifacts/Dataset"
+#frames_path = r"/home/silvia.collicelli/frames"
+clinical_path = r"C:\Users\utente\Documents\UNI\MAGISTRALE\tesi\naive_baseline\raw_dataset\artifacts\clinical_case_metadata.parquet"
+folder_path = r"C:\Users\utente\Documents\UNI\MAGISTRALE\tesi\naive_baseline\raw_dataset\artifacts\Dataset"
 
-print("start dataset")
 data = MyDataset(clinical_path, folder_path)
 print("done dataset")
 
-print(data.samples[-1])
+random.shuffle(data.case_folders)
+train_cases = data.case_folders[:int(0.8*len(data.case_folders))]
+test_cases = data.case_folders[int(0.8*len(data.case_folders)):]
+
+train_idx = []
+test_idx = []
+
+for i, p in enumerate(data.samples):
+    if p[2] in train_cases:
+        train_idx.append(i)
+    else: 
+        test_idx.append(i)
+
+train_dataset = torch.utils.data.Subset(data, train_idx)
+test_dataset  = torch.utils.data.Subset(data, test_idx)
+print("divided into training and validation per patient")
